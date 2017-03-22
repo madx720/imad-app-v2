30e8836a93d2cdf6fdb4eca9db7018325e9206af
madx720 <maharsh.dave@gmail.com>
2017-03-22 16:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,12 +26,38 @@ var articleOne={
     `
     
 };
-
+function createTemplate(data){
+    var title= data.title;
+    var date = data.date;
+    var heading =data.heading;
+    var content=data.content;
+var htmlTemplate =`<html>
+  <head>
+      <title>
+          ${title}
+          </title>
+      </head>  
+      
+<body>
+    <div>
+        <a href="/">Home</a>
+         </div>
+        <hr/>
+        <h3>${date}</h3>
+        <div>
+            ${content}      
+            </div>
+    
+    </body>      
+   
+</html>`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
    
 });
 app.get('/ui/style.css', function (req, res) {
