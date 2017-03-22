4422b464206b773e61d3b7be82a30f4acdd1ff03
madx720 <maharsh.dave@gmail.com>
2017-03-22 16:19:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.send('Article-One is to be served here'); 
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
