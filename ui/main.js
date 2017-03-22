2446c780f517b31a325bc4d38c537a7b42f7891f
madx720 <maharsh.dave@gmail.com>
2017-03-22 22:29:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 console.log('Loaded!');
 //MOve the image
 var img = document.getElementById('madi');
-img.onclick= function(){
+var marginLeft =0;
+function moveRight(){
+    marginLeft =marginLeft +10;
+    img.style.marginLeft =marginLeft+'px';
+}
+img.onclick= function (){
+    var interval= setInterval(moveRight,100); 
     img.style.marginLeft ='100px';
 }
 ;
