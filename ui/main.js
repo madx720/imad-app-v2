80caf6dceed33b8171239d9fe329854d09819972
madx720 <maharsh.dave@gmail.com>
2017-03-25 15:28:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,27 @@
 //counter code
 var button=document.getElementById('counter');
 var counter=0;
-button.onClick=function(){
-  counter =counter+1;
-  var span = document.getElementById('count');
+button.onClick=function (){
+    
+    
+var request    = new XMLHttpRequest();
+request.onreadystatechange = function (){
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status ===200){
+            var counter=request.responseText;
+    var span = document.getElementById('count');
   span.innerHTML = counter.toString();
   
+            
+        }
+    }
+    
+    
+    
+} ;
+ request.open('GET','http://madx720.imad.hasura-app.io/counter',true);
+ request.send(null);
+ 
     
     
     
